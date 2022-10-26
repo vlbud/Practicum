8094d61a73a7e40e793eefc5ddf76836f03c93e4
vlabud <buyou.self@mail.ru>
2022-10-26 20:08:50 +0300

    init

---
+++ Тема 3/Лабораторные работы/task1/main.py
@@ -0,0 +1,9 @@
+src = not False and True or False and not True
+
+# TODO расписать упрощение выражения
+True and True or False and not False
+True or False
+True
+result = True # TODO подставить результат выражения
+
+print(src == result)
